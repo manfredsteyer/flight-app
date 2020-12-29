e6f1d9089c3b3fb36018b75495dd788360a1007b
Manfred Steyer <manfred.steyer@gmx.net>
2020-12-29 15:34:55 +0100

    K3 fertig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+// src/app/app.module.ts
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
    imports: [
+      FormsModule,
+      HttpClientModule,
       BrowserModule
    ],
    declarations: [
       AppComponent,
       SidebarComponent,
-      NavbarComponent
+      NavbarComponent,
+      FlightSearchComponent
    ],
    providers: [],
    bootstrap: [
---
+++ src/app/flight-search/flight-search.component.ts
@@ -0,0 +1,76 @@
+// src/app/flight-search/flight-search.component.ts
+
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Flight } from '../flight';
+
+@Component({
+  selector: 'app-flight-search',
+  templateUrl: './flight-search.component.html',
+  styleUrls: ['./flight-search.component.scss']
+})
+export class FlightSearchComponent implements OnInit {
+
+  from = 'Hamburg';
+  to = 'Graz';
+  flights: Array<Flight> = [];
+  selectedFlight: Flight | null = null;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+
+    this.createDemoFlight();
+
+  }
+
+  createDemoFlight(): void {
+    const url = 'http://www.angular.at/api/flight';
+
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json');
+
+    const newFlight: Flight = {
+      id: 0,
+      from: 'Gleisdorf',
+      to: 'Graz',
+      date: new Date().toISOString()
+    };
+
+    this.http.post<Flight>(url, newFlight, { headers }).subscribe({
+      next: (flight) => {
+        console.debug('Neue Id: ', flight.id);
+      },
+      error: (err) => {
+        console.debug('Error', err);
+      }
+    });
+  }
+
+  search(): void {
+
+    const url = 'http://www.angular.at/api/flight';
+
+    const headers = new HttpHeaders()
+      .set('Accept', 'application/json');
+
+    const params = new HttpParams()
+      .set('from', this.from)
+      .set('to', this.to);
+
+    this.http.get<Flight[]>(url, {headers, params}).subscribe({
+      next: (flights) => {
+        this.flights = flights;
+      },
+      error: (err) => {
+        console.debug('Error', err);
+      }
+    });
+  }
+
+  select(f: Flight): void {
+    this.selectedFlight = f;
+  }
+
+}
