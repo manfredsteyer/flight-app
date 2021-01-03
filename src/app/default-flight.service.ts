938ef5a7b00cdaf4b605767327ff886226f206ce
Manfred Steyer <manfred.steyer@gmx.net>
2021-01-03 12:32:52 +0100

    use life-cycle-hook for service

--- src/app/default-flight.service.ts
+++ src/app/default-flight.service.ts
@@ -1,15 +1,19 @@
 // src/app/default-flight.service.ts
 
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Flight } from './flight';
 import { FlightService } from './flight.service';
 
 @Injectable()
-export class DefaultFlightService implements FlightService {
+export class DefaultFlightService implements FlightService, OnDestroy {
 
   constructor(private http: HttpClient) { }
+
+  ngOnDestroy(): void {
+    console.debug('Sag beim Abschied leise Servus!');
+  }
 
   find(from: string, to: string): Observable<Flight[]> {
     const url = 'http://www.angular.at/api/flight';
