cd7ebec37a013236e1206b231df15d7f96094a00
Manfred Steyer <manfred.steyer@gmx.net>
2021-05-15 14:37:48 +0200

    typeahead

--- src/app/flight-lookahead/flight-typeahead.component.ts
+++ src/app/flight-lookahead/flight-typeahead.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { FlightLookaheadService } from './flight-typeahead.service';
+import { Observable } from 'rxjs';
+import { debounceTime, filter, switchMap } from 'rxjs/operators';
+import { Flight } from '../flight-booking/flight';
+import { FlightService } from '../flight-booking/flight.service';
 
 @Component({
     selector: 'app-flight-typeahead',
@@ -8,17 +11,27 @@ import { FlightLookaheadService } from './flight-typeahead.service';
 })
 export class FlightLookaheadComponent implements OnInit {
 
-    flights$ = this.service.flights$;
-    online$ = this.service.online$;
+    // Quelle
     control = new FormControl();
+    input$ = this.control.valueChanges;
 
-    constructor(private service: FlightLookaheadService) {
+    // Ziel
+    flights$: Observable<Flight[]>;
+
+    constructor(private flightService: FlightService) {
+        this.flights$ = this.input$.pipe(
+            filter(value => value.length >= 3),
+            debounceTime(300),
+            switchMap(input => this.load(input))
+        );
+    }
+
+    // Noch eine Quelle
+    load(from: string): Observable<Flight[]> {
+        return this.flightService.find(from, '');
     }
 
     ngOnInit(): void {
-        this.control.valueChanges.subscribe(from => {
-            this.service.search(from);
-        });
     }
 
 }
