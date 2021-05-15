1ee246113e245af57c7ddd06de8fb4bd548dfeda
Manfred Steyer <manfred.steyer@gmx.net>
2021-05-15 18:40:26 +0200

    done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Flight } from '../flight-booking/flight';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+
+    this.http.get<Flight>('http://www.angular.at/api/flight/3')
+      .pipe(
+        map(flight => new Date(flight.date)),
+        // Hier könnten weitere Operatoren stehen
+      )
+      .subscribe({
+        next: value => console.log('date', value),
+        error: err => console.error('error', err),
+        complete: () => console.log('complete')
+      });
+
+    const subject = new Subject<string>();
+    subject.next('A');
+    subject.subscribe(value => console.debug('Subject says', value));
+    subject.next('B');
+    subject.next('C');
+
+    const sender = new BehaviorSubject<string>('init');
+    const receiver$ = sender.asObservable();
+    receiver$.subscribe(value => console.debug('Subject says', value));
+    sender.next('A');
+    sender.next('B');
+    sender.next('C');
+
+    const simple$ = simpleObservable();
+    simple$.subscribe({
+      next: value => console.log('next', value),
+      error: err => console.error('error', err),
+      complete: () => console.log('complete')
+    });
+
+    const interval$ = simpleInterval();
+    const sub = interval$.subscribe({
+      next: value => console.log('next', value),
+      error: err => console.error('error', err),
+      complete: () => console.log('complete')
+    });
+
+    setTimeout(() => sub.unsubscribe(), 2500);
   }
 
 }
---
+++ src/app/shared/observable-factories.ts
@@ -0,0 +1,45 @@
+//
+// These examples are provided for illustrative purposes only.
+// In practice, one often falls back on existing factories
+// ("creation operators") instead.
+//
+
+import { Observable } from 'rxjs';
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+export function simpleObservable(): Observable<number> {
+    return new Observable<number>(observer => {
+
+        observer.next(4711);
+        observer.next(815);
+        observer.error('Manfred braucht einen Kaffee!');
+        observer.complete();
+
+        return () => {
+            console.log(`Sag' zum Abschied leise Servus!`);
+        };
+    });
+}
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+export function simpleInterval(): Observable<number> {
+    return new Observable<number>(observer => {
+        let counter = 0;
+
+        const handle = setInterval(() => {
+            observer.next(counter);
+            // observer.error('Habe mich verzählt ...')
+
+            counter++;
+            if (counter >= 3) {
+                clearInterval(handle);
+                observer.complete();
+            }
+        }, 1000);
+
+        return () => {
+            clearInterval(handle);
+            console.log(`Sag' zum Abschied leise Servus!`);
+        };
+    });
+}
