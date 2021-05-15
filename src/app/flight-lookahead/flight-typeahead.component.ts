db7162c13481cc9e9124ed46902424045a5c06bd
Manfred Steyer <manfred.steyer@gmx.net>
2021-05-15 14:57:16 +0200

    online$

--- src/app/flight-lookahead/flight-typeahead.component.ts
+++ src/app/flight-lookahead/flight-typeahead.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { debounceTime, filter, switchMap } from 'rxjs/operators';
+import { combineLatest, interval, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, startWith, switchMap, tap } from 'rxjs/operators';
 import { Flight } from '../flight-booking/flight';
 import { FlightService } from '../flight-booking/flight.service';
 
@@ -11,19 +11,38 @@ import { FlightService } from '../flight-booking/flight.service';
 })
 export class FlightLookaheadComponent implements OnInit {
 
-    // Quelle
+    // Quellen
     control = new FormControl();
     input$ = this.control.valueChanges;
+
+    // Nicht schön. Wir reden später darüber.
+    online = false;
+
+    online$ = interval(2000).pipe(
+        startWith(-1),
+        map(() => Math.random() < 0.5),
+        distinctUntilChanged(),
+        tap(online => {
+            this.online = online;
+        })
+    );
 
     // Ziel
     flights$: Observable<Flight[]>;
 
     constructor(private flightService: FlightService) {
-        this.flights$ = this.input$.pipe(
+
+        const debouncedInput$ = this.input$.pipe(
             filter(value => value.length >= 3),
             debounceTime(300),
+        );
+
+        this.flights$ = combineLatest([debouncedInput$, this.online$]).pipe(
+            filter(([_, online]) => online),
+            map(([input, _]) => input),
             switchMap(input => this.load(input))
         );
+
     }
 
     // Noch eine Quelle
