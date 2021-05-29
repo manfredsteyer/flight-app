dab6e70b4ced940656a9d2935504e0d4ddf32bbb
Manfred Steyer <manfred.steyer@gmx.net>
2021-05-29 15:46:29 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import './app/ts/demo';
 
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, LOCALE_ID, Provider, StaticProvider } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import it from '@angular/common/locales/it';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(it);
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+const providers: StaticProvider[] = [
+  { provide: LOCALE_ID, useValue: 'it' }
+];
+
+platformBrowserDynamic().bootstrapModule(AppModule, { providers })
   .catch(err => console.error(err));
